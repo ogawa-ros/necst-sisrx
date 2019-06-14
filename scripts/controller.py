36ef5d3263e9742f460a55dbf198db2b420835f4
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-06-14 15:21:01 +0900

    update controller

--- scripts/controller.py
+++ scripts/controller.py
@@ -11,100 +11,97 @@ import std_msgs.msg
 class controller(object):
 
     def __init__(self):
-        self.ps = PS()
-        self.sis = SIS()
-        self.irrsg = IRRSG()
-        self.lo1st = LO1ST()
-        self.loatt = LOATT()
+        self.sis = sis()
+        self.irrsg = irrsg()
+        self.lo1st = lo1st()
+        self.loatt = loatt()
 
 
 
-class PS(object):
+class make_pub(object):
 
     def __init__(self):
 
         self.pub = {}
         pass
 
-    def publish(self, topic_name, msg):
-        self.pub[topic_name].publish(msg)
+    def publish(self, topic_name, data_class, msg):
+        if self.pub[topic_name]:
+            self.pub[topic_name].publish(msg)
+        else:
+            self.set_publisher(topic_name = topic_nema, data_class = data_class)
+            self.pub[topic_name].publish(msg)
         return
 
-    def set_publisher(self, topic_name, data_class, queue_size, latch = True):
+    def set_publisher(self, topic_name, data_class):
         if topic_name not in self.pub:
-            self.pub[topic_name] = rospy.Publisher(name = topic_name, data_class = data_class, queue_size =queue_size, latch = latch)
+            self.pub[topic_name] = rospy.Publisher(name = topic_name, data_class = data_class, queue_size = 1, latch = False)
             time.sleep(0.01)
         else:
             pass
         return
 
 
-class SIS(object):
+class sis(object):
 
     def __init__(self):
         rospy.init_node(name)
         self.ps = PS()
 
-    def sis_vp(self, command):
+    def set_sis_vp(self, command):
         topic_name = '/necst_sisrx/sis/vp_cmd'
+        data_class = std_msgs.msg.Float64
 
-        self.ps.set_publisher(topic_name = topic_name, data_class = std_msgs.msg.Float64, queue_size = 1, latch = True)
-
-        self.ps.publish(topic_name = topic_name, msg = command)
+        self.ps.publish(topic_name, data_class, msg = command )
         return
 
 
-class IRRSG(object):
+class irrsg(object):
 
     def __init__(self):
         rospy.init_node(name)
         self.ps = PS()
 
-    def irrsg_f(self, command):
+    def set_irrsg_freq(self, command):
         topic_name = '/necst_sisrx/irrsg/f_cmd'
+        data_class = std_msgs.msg.Float64
 
-        self.ps.set_publisher(topic_name = topic_name, data_class = std_msgs.msg.Float64, queue_size = 1, latch = True)
+        self.ps.publish(topic_name, data_class, msg = command)
 
-        self.ps.publish(topic_name = topic_name, msg = command)
-
-    def irrsg_p(self, command):
+    def set_irrsg_power(self, command):
         topic_name = '/necst_sisrx/irrsg/p_cmd'
+        data_class = std_msgs.msg.Float64
 
-        self.ps.set_publisher(topic_name = topic_name, data_class = std_msgs.msg.Float64, queue_size = 1, latch = True)
-
-        self.ps.publish(topic_name = topic_name, msg = command)
+        self.ps.publish(topic_name, data_class, msg = command)
 
 
-class LO1ST(object):
+class lo1st(object):
 
     def __init__(self):
         rospy.init_node(name)
         self.ps = PS()
 
-    def lo1st_f(self, command):
+    def set_lo1st_freq(self, command):
         topic_name = '/necst_sisrx/lo1st/f_cmd'
+        data_class = std_msgs.msg.Float64
 
-        self.ps.set_publisher(topic_name = topic_name, data_class = std_msgs.msg.Float64, queue_size = 1, latch = True)
+        self.ps.publish(topic_name, data_class, msg = command)
 
-        self.ps.publish(topic_name = topic_name, msg = command)
-
-    def lo1st_p(self, command):
+    def set_lo1st_power(self, command):
         topic_name = '/necst_sisrx/lo1st/p_cmd'
+        data_class = std_msgs.msg.Float64
 
-        self.ps.set_publisher(topic_name = topic_name, data_class = std_msgs.msg.Float64, queue_size = 1, latch = True)
-
-        self.ps.publish(topic_name = topic_name, msg = command)
+        self.ps.publish(topic_name, data_class, msg = command)
 
 
-class LOATT(object):
+class loatt(object):
 
     def __init__(self):
         rospy.init_node(name)
         self.ps = PS()
 
-    def loatt_v(self, command):
+    def set_loatt_vol(self, command):
         topic_name = '/necst_sisrx/loatt/v_cmd'
+        data_class = std_msgs.msg.Float64
 
-        self.ps.set_publisher(topic_name = topic_name, data_class = std_msgs.msg.Float64, queue_size = 1, latch = True)
-
-        self.ps.publish(topic_name = topic_name, msg = command)
+        self.ps.publish(topic_name, data_class, msg = command)
