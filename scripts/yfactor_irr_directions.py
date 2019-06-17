120cbc3ffdf27e85802a63f3f6ad61fa1877ffe1
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-06-17 20:54:13 +0900

    add yfactor and irr directions

---
+++ scripts/yfactor_irr_directions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+name = 'yfactor_irr_directions'
+
+
+import rospy
+
+import controller
+import core_controller
+
+sisrx_node_name = 'sisrx_controller'
+core_node_name = 'core_controller'
+rospy.init_node(sisrx_node_name)
+rospy.init_node(core_node_name)
+
+sis = controller.sis()
+irrsg = controller.irrsg()
+lo = controller.lo1st()
+loatt = controller.loatt()
+logger = core_controller.logger()
+
+lo.set_lo1st_freq(freq)
+lo.set_lo1st_power(power)
+
+sis.set_sis_vp(0)
+loatt.set_loatt_vol(att_vol)
+logger.logger(iv)
+
+sis.set_sis_vp(volp)
+logger.logger(yfactor)     #have to repeat until determining optimal voltage value
+
+loatt.set_loatt_vol(att_vol)   #necessary anather loatt_vol?
+logger.logger(yfactor)     #have to repeat until determining optimal att voltage value
+
+logger.logger(irr)
