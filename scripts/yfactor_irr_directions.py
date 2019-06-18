257258d677d04192cf1024b08c1845a88bb233aa
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-06-18 18:15:30 +0900

    add yfactor and irr directions

--- scripts/yfactor_irr_directions.py
+++ scripts/yfactor_irr_directions.py
@@ -11,12 +11,7 @@ import controller
 import core_controller
 import tz2019_controller
 
-sisrx_node_name = 'sisrx_controller'
-core_node_name = 'core_controller'
-tz2019_node_name = 'tz2019_controller'
-rospy.init_node(sisrx_node_name)
-rospy.init_node(core_node_name)
-rospy.init_node(tz2019_node_name)
+rospy.init_node(name)
 
 sis = controller.sis()
 irrsg = controller.irrsg()
@@ -50,10 +45,11 @@ while question is not 'OK':
     for band in bands:              #x4 repeat hu,hl,vu,vl
         switch_value = band
         switch.set_if_switch(switch_value)
+        input("Are you ready for hot measure?: ")
         logger.start(hot_band)
         time.sleep()
         logger.stop()
-        time.sleep()
+        input("Are you ready for cold measure?: ")
         logger.start(cold_band)
         time.sleep()
         logger.stop()     #have to repeat until determining optimal voltage value
@@ -75,52 +71,21 @@ while question is not 'OK':
         logger.stop()    #have to repeat until determining optimal att voltage value
     question = input("Are you OK or NOT?: ")
 
-irr_freq_low = input("IRRSG lower freq: ")   #measure irr of h side
-irr_power_low = input("IRRSG lower power: ")
-irrsg.set_irrsg_freq(irr_freq_low)
-irrsg.set_irrsg_power(irr_power_low)
-switch.set_if_switch(hl)
-logger.start(irr_low)
-time.sleep()
-logger.stop()
-switch.set_if_switch(hu)
-logger.start(irr_low)
-time.sleep()
-logger.stop()
-irr_freq_up = input("IRRSG upper freq: ")
-irr_power_up = input("IRRSG upper power: ")
-irrsg.set_irrsg_freq(irr_freq_up)
-irrsg.set_irrsg_power(irr_power_up)
-switch.set_if_switch(hl)
-logger.start(irr_up)
-time.sleep()
-logger.stop()
-switch.set_if_switch(hu)
-logger.start(irr_up)
-time.sleep()
-logger.stop()
-
-irr_freq_low = input("IRRSG lower freq: ")     #measure irr of v side
-irr_power_low = input("IRRSG lower power: ")
-irrsg.set_irrsg_freq(irr_freq_low)
-irrsg.set_irrsg_power(irr_power_low)
-switch.set_if_switch(vl)
-logger.start(irr_low)
-time.sleep()
-logger.stop()
-switch.set_if_switch(vu)
-logger.start(irr_low)
-time.sleep()
-logger.stop()
-irr_freq_up = input("IRRSG upper freq: ")
-irr_power_up = input("IRRSG upper power: ")
-irrsg.set_irrsg_freq(irr_freq_up)
-irrsg.set_irrsg_power(irr_power_up)
-switch.set_if_switch(vl)
-logger.start(irr_up)
-time.sleep()
-logger.stop()
-switch.set_if_switch(vu)
-logger.start(irr_up)
-time.sleep()
-logger.stop()
+pols = ['h', 'v']
+for pol in pols:
+    sides = {'upper', 'lower':}
+    for side in sides:
+        irr_freq = input("IRRSG %s freq: "%(side))   #measure irr of h side
+        irr_power = input("IRRSG %s power: "%(side))
+        irrsg.set_irrsg_freq(irr_freq)
+        irrsg.set_irrsg_power(irr_power)
+        if pol is 'h':
+            bands = ['hu', 'hl']
+        elif pol is 'v':
+            bands = ['vu', 'vl']
+        for band in bands:
+            switch.set_if_switch(band)
+            input("Are you ready for IRR %s measure: "%(band))
+            logger.start(band)
+            time.sleep()
+            logger.stop()
