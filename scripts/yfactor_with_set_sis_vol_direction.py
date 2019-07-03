cd26eda69c530d0942b7d980c001d9f3ae133533
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-07-03 14:13:29 +0900

    update set-sis-vol-direction

--- scripts/yfactor_with_set_sis_vol_direction.py
+++ scripts/yfactor_with_set_sis_vol_direction.py
@@ -22,14 +22,16 @@ switch = tz2019_controller.switch()
 parser = argparse.ArgumentParser(description = 'search optical sis voltage value')
 
 parser.add_argument('switch_value', choices = ['hu', 'hl', 'vu', 'vl'], type = float, help = 'choice IF output port')
+parser.add_argument('save_name', type = string, help = 'set saving file name')
 
 args = parser.parse_args()
 
+file_name = '/home/exito/data/logger/%s'%(save_name)
 volp1 = np.linespace(-1, 0, 5)   #search optimal SIS voltage value
 volp2 = np.linespace(-1, 0, 5)
 switch.set_if_switch(switch_value)
 loatt.set_loatt_vol(att)
-logger.start(yfactor)
+logger.start(file_name)
 for vp1 in volp1:             #measure hot
     sis.set_sis_vp(vp1)
     for vp2 in volp2:
