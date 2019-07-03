99edd38f1995bc2ef9f6330cf2298c6d6be6b755
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-07-03 17:04:12 +0900

    update directions

--- scripts/yfactor_with_set_loatt_level_direction.py
+++ scripts/yfactor_with_set_loatt_level_direction.py
@@ -23,14 +23,15 @@ switch = tz2019_controller.switch()
 parser = argparse.ArgumentParser(description = 'search optical Lo Att voltage value')
 
 parser.add_argument('switch_value', choices = ['hu', 'hl', 'vu', 'vl'], type = float, help = 'choice IF output port')
+parser.add_argument('sis_v', type = float, help = 'set optical SIS voltage value')
 parser.add_argument('save_name', type = string, help = 'set saving file name')
 
 args = parser.parse_args()
 
-file_name = '/home/exito/data/logger/%s'%(save_name)
+file_name = '/home/exito/data/logger/%s'%(args.save_name)
 att_vol = np.arange(21)    #search optimal Lo Att level
-switch.set_if_switch(switch_value)
-sis.set_sis_vp(sis_v)
+switch.set_if_switch(args.switch_value)
+sis.set_sis_vp(args.sis_v)
 logger.start(file_name)
 for att_v in att_vol:           #measure hot
     loatt.set_loatt_vol(att_v)
