f41f6041a2f421da159cb2694d46328e56d36320
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-07-12 15:46:38 +0900

    test

--- scripts/sis_i-v_curve_direction.py
+++ scripts/sis_i-v_curve_direction.py
@@ -27,7 +27,7 @@ args = parser.parse_args()
 
 file_name = '/home/exito/data/logger/test/%s'%(args.save_name)
 print(file_name)
-logger.start('test.db')
+logger.start('')
 time.sleep(10)
 logger.start(file_name)
 '''
@@ -36,6 +36,6 @@ for vgap in sis_vgap:
     sis.set_vgap(vgap)
     time.sleep(0.1)
     continue
+'''
 time.sleep(10)
 logger.stop()
-'''
