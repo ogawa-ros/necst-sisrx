e0a8135d925928cb318bad4e250db7ff24c5325b
tsutsumi-toshihisa <104.toshi.223@gmail.com>
2019-07-23 15:04:48 +0900

    test_add

--- scripts/test.py
+++ scripts/test.py
@@ -35,32 +35,32 @@ att1.set_cur(10)
 time.sleep(60)
 att2.set_cur(10)
 print("att_level = 10")
-    sis_vgap = numpy.arange(0, 1.2, 0.001)
-    for vgap in sis_vgap:
-        sis.set_vgap(vgap)
-        time.sleep(0.05)
-        continue
+sis_vgap = numpy.arange(0, 1.2, 0.001)
+for vgap in sis_vgap:
+    sis.set_vgap(vgap)
+    time.sleep(0.05)
+    continue
 
 att1.set_cur(20)
 time.sleep(60)
 att2.set_cur(20)
 print("att_level = 20")
-    sis_vgap = numpy.arange(0, 1.2, 0.001)
-    for vgap in sis_vgap:
-        sis.set_vgap(vgap)
-        time.sleep(0.05)
-        continue
+sis_vgap = numpy.arange(0, 1.2, 0.001)
+for vgap in sis_vgap:
+    sis.set_vgap(vgap)
+    time.sleep(0.05)
+    continue
 
 
 att1.set_cur(30)
 time.sleep(60)
 att2.set_cur(30)
 print("att_level = 30")
-    sis_vgap = numpy.arange(0, 1.2, 0.001)
-    for vgap in sis_vgap:
-        sis.set_vgap(vgap)
-        time.sleep(0.05)
-        continue
+sis_vgap = numpy.arange(0, 1.2, 0.001)
+for vgap in sis_vgap:
+    sis.set_vgap(vgap)
+    time.sleep(0.05)
+    continue
 
 sis.set_vgap(0)
 logger.stop()
