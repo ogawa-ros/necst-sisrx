68ce98cac8fa5401c90c034081af3686169aa310
tsutsumi-toshihisa <104.toshi.223@gmail.com>
2019-07-25 15:21:55 +0900

    new_app

--- scripts/sis_i-v_curve_with_search_first_loatt_level_direction.py
+++ scripts/sis_i-v_curve_with_search_first_loatt_level_direction.py
@@ -19,6 +19,9 @@ import controller
 import core_controller
 import tz2019_controller
 
+sys.path.append("/home/exito/ros/src/necst-tz2019/scripts")
+import plot_tool
+
 rospy.init_node(name)
 
 sis = controller.sis()
@@ -51,49 +54,8 @@ for att_vol in att:
     #time.sleep(60)
     logger.stop()
 
+    plot_tool.att_iv_plot(file_name, args.save_name, att_vol)
 
-    db = necstdb.necstdb()  #plot graph
-    db.open(file_name)
-
-    d = db.read_as_pandas()
-    d['time'] = pandas.to_datetime(d['time'], unit='s')
-    d['data'] = [_[0]['data'] for _ in d['msgs']]
-    d2 = d.set_index(['topic', 'time']).sort_index()
-
-    dd = pandas.concat(
-        [
-            d2.loc['/tz2019/sis_v1/v'][['data']].rename(columns={'data': 'V1V'}).astype(float).resample('0.1S').mean(),
-            d2.loc['/tz2019/sis_v1/i'][['data']].rename(columns={'data': 'V1I'}).astype(float).resample('0.1S').mean(),
-            d2.loc['/tz2019/sis_h1/v'][['data']].rename(columns={'data': 'H1V'}).astype(float).resample('0.1S').mean(),
-            d2.loc['/tz2019/sis_h1/i'][['data']].rename(columns={'data': 'H1I'}).astype(float).resample('0.1S').mean(),
-            d2.loc['/tz2019/sis_v2/v'][['data']].rename(columns={'data': 'V2V'}).astype(float).resample('0.1S').mean(),
-            d2.loc['/tz2019/sis_v2/i'][['data']].rename(columns={'data': 'V2I'}).astype(float).resample('0.1S').mean(),
-            d2.loc['/tz2019/sis_h2/v'][['data']].rename(columns={'data': 'H2V'}).astype(float).resample('0.1S').mean(),
-            d2.loc['/tz2019/sis_h2/i'][['data']].rename(columns={'data': 'H2I'}).astype(float).resample('0.1S').mean(),
-        ],
-        axis = 1,
-    )
-
-    #under the i-v graph plot
-
-    fig = plt.figure(figsize=(7, 6))
-    ax = [fig.add_subplot(2, 2, i) for i in range(1, 5)]
-    ax[0].plot(dd['V1V'], dd['V1I'], '.')
-    ax[1].plot(dd['H1V'], dd['H1I'], '.')
-    ax[2].plot(dd['V2V'], dd['V2I'], '.')
-    ax[3].plot(dd['H2V'], dd['H2I'], '.')
-    ax[0].set_title('V1 I-V')
-    ax[1].set_title('H1 I-V')
-    ax[2].set_title('H2 I-V')
-    ax[3].set_title('V2 I-V')
-    [_.set_xlabel('Voltage (mV)') for _ in ax]
-    [_.set_ylabel('Current (uA)') for _ in ax]
-    [_.grid(True, linestyle=':') for _ in ax]
-
-    plt.suptitle('att_level = %d'%(att_vol))
-    #plt.show()
-    graph_file_name = '/home/exito/data/logger/test/' + str(args.save_name) + '/att_level=' + str(att_vol) +'.png'
-    plt.savefig(graph_file_name)
     continue
 
 sis.set_vgap(0)
