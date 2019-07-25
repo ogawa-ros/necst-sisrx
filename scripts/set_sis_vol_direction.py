4bdbe4e0611755f3cf6b38dc8b18add3585c7556
tsutsumi-toshihisa <104.toshi.223@gmail.com>
2019-07-25 21:31:45 +0900

    improve

--- scripts/set_sis_vol_direction.py
+++ scripts/set_sis_vol_direction.py
@@ -6,12 +6,18 @@ import rospy
 import time
 import std_msgs.msg
 import argparse
-
 import controller
+
+sys.path.append("/home/exito/ros/src/necst-core/scripts")
+import core_controller
+
+sys.path.append("/home/exito/ros/src/necst-tz2019/scripts")
+import plot_tool
 
 rospy.init_node(name)
 
 sis = controller.sis()
+logger = core_controller.logger()
 
 parser = argparse.ArgumentParser(description = 'set SIS voltage V1 and V2')
 
@@ -19,6 +25,7 @@ parser.add_argument('v1', type = float, help = 'set SIS voltage of V1')
 parser.add_argument('h1', type = float, help = 'set SIS voltage of V2')
 parser.add_argument('h2', type = float, help = 'set SIS voltage of V1')
 parser.add_argument('v2', type = float, help = 'set SIS voltage of V2')
+parser.add_argument('save_name', type = str, help = 'set saving file name')
 
 args = parser.parse_args()
 
@@ -26,3 +33,10 @@ sis.set_v1(args.v1)
 sis.set_h1(args.h1)
 sis.set_h2(args.h2)
 sis.set_v2(args.v2)
+
+file_name = '/home/exito/data/logger/test/%sset_vol_data.db'%(args.save_name, att_vol)
+print(file_name)
+logger.start(file_name)
+time.sleep(10)
+logger.stop()
+plot_tool.iv_plot(file_name, args.save_name)
