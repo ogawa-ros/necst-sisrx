58dbca79ce4c9410edbfaf7fdd878536c3ab1fb0
tsutsumi-toshihisa <104.toshi.223@gmail.com>
2019-07-29 21:59:44 +0900

    improve

--- scripts/yfactor_direction.py
+++ scripts/yfactor_direction.py
@@ -9,8 +9,10 @@ import std_msgs.msg
 import argparse
 
 sys.path.append("/home/exito/ros/src/necst-core/scripts")
-
 import core_controller
+
+sys.path.append("/home/exito/ros/src/necst-tz2019/scripts")
+import plot_tool
 
 rospy.init_node(name)
 
@@ -22,7 +24,9 @@ parser.add_argument('save_name', type = str, help = 'set saving file name')
 
 args = parser.parse_args()
 
-file_name = '/home/exito/data/logger/%s'%(args.save_name)
+file_name = '/home/exito/data/logger/%s.db'%(args.save_name)
 logger.start(file_name)
 time.sleep(30)
 logger.stop()
+
+plot_tool.yfactor_prot(file_name, args.save_name)
