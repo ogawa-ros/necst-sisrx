1104f0dfe2832df5ef60c65f55c463c91fa113db
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-07-31 20:10:46 +0900

    fix

--- scripts/set_sis_vol_and_loatt_level_direction.py
+++ scripts/set_sis_vol_and_loatt_level_direction.py
@@ -10,12 +10,11 @@ import argparse
 
 sys.path.append("/home/exito/ros/src/necst-tz2019/scripts")
 
-import controller
 import tz2019_controller
 
 rospy.init_node(name)
 
-sis = controller.sis()
+sis = tz2019_controller.sis()
 att1 = tz2019_controller.loatt_h()
 att2 = tz2019_controller.loatt_v()
 
@@ -30,10 +29,10 @@ parser.add_argument('att2', type = float, help = 'set Lo Att. level of V-side')
 
 args = parser.parse_args()
 
-sis.set_v1(args.v1)
-sis.set_h1(args.h1)
-sis.set_h2(args.h2)
-sis.set_v2(args.v2)
+sis.set_v1_v(args.v1)
+sis.set_h1_v(args.h1)
+sis.set_h2_v(args.h2)
+sis.set_v2_v(args.v2)
 att1.set_cur(args.att1)
 time.sleep(60)
 att2.set_cur(args.att2)
--- scripts/set_sis_vol_direction.py
+++ scripts/set_sis_vol_direction.py
@@ -4,14 +4,17 @@ name = 'set_sis_vol_direction'
 
 import rospy
 import time
+import sys
 import std_msgs.msg
 import argparse
-import controller
-import time
+
+sys.path.append("/home/exito/ros/src/necst-tz2019/scripts")
+
+import tz2019_controller
 
 rospy.init_node(name)
 
-sis = controller.sis()
+sis = tz2019_controller.sis()
 
 parser = argparse.ArgumentParser(description = 'set SIS voltage V1 and V2')
 
@@ -22,7 +25,7 @@ parser.add_argument('v2', type = float, help = 'set SIS voltage of V2')
 
 args = parser.parse_args()
 
-sis.set_v1(args.v1)
-sis.set_h1(args.h1)
-sis.set_h2(args.h2)
-sis.set_v2(args.v2)
+sis.set_v1_v(args.v1)
+sis.set_h1_v(args.h1)
+sis.set_h2_v(args.h2)
+sis.set_v2_v(args.v2)
