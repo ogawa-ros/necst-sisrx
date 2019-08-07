2cd2ea68dff67d5863e778a99f14eb97c3120200
tsutsumi-toshihisa <104.toshi.223@gmail.com>
2019-08-07 16:28:39 +0900

    improve

--- scripts/plot_tool.py
+++ scripts/plot_tool.py
@@ -176,7 +176,7 @@ def sis_bias_and_yfactor_matrix_plot(v1, v2, trx, save_name):
     )
     np.save(
     '/home/exito/data/logger/test/'+str(save_name)+'/v2_data',
-    x
+    y
     )
     np.save(
     '/home/exito/data/logger/test/'+str(save_name)+'/trx_data',
